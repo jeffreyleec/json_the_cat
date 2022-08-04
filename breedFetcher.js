976911713483965d3e600fbd3bed013201109e38
Jeffrey <jeffrey-lee@live.ca>
2022-08-04 20:20:31 +0000

    breedFetcherTest

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,30 @@
-const https = require('https');
-const myArgs = process.argv.slice(2);
-const breedName = myArgs[0];
-//console.log(breedName)
+//const https = require('https');
+//const breedName = require('./index.js');
 
 const request = require('request');
 
-request(`https://api.thecatapiappleorangedog.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (error) {
-    console.log(error, 'testetsetset'); // Print the error if one occurred
 
-  } else {
-    const data = JSON.parse(body);
-    if (data[0] === undefined) {
-      console.log('check your spelling');
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      
+      return callback(error, null); // Print the error if one occurred
+  
     } else {
-      console.log(data[0].description);
-
+      const data = JSON.parse(body);
+  
+      if (data[0] === undefined) {
+        return callback('check your spelling');
+      } else {
+        return callback(null, data[0].description);
+    
+      }
     }
-  }
-
-
    
-});
+  });
+ 
+  
+};
 
 
-
-
+module.exports = { fetchBreedDescription };
